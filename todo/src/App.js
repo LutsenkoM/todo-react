4fd233e260ed37bb852ad5d65dff6a8a0e0ed162
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-08-22 19:03:39 +0300

    Add data to todo, refactoring

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,11 +5,18 @@ import SearchPanel from './components/search-panel'
 import TodoList from './components/todo-list'
 
 function App() {
+
+  const todoData = [
+    { id:1, label: 'Drink Coffee', important: false},
+    { id:2, label: 'Create Awesome App', important: true},
+    { id:3, label: 'Buy milk', important: false}
+  ];
+
   return (
     <div className="App">
-      <AppHeader/>
-      <SearchPanel/>
-      <TodoList/>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos={todoData} />
     </div>
   );
 }
